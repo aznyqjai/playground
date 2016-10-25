3b1063e3ef7fb9a2997653c2b912f0c170b4e32d
Q. Zhang <aznyqjai@gmail.com>
2016-10-25 10:10:06 -0400

    divisible by 7

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("playground is up and running");
+// console.log("playground is up and running");
 
+var num=100;
+while (num>0){
+	if (num%7===0){
+		console.log("Lucky me!");
+	}
+	else {
+		console.log(num);
+	}
+	num--;
+}
