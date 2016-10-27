32cfe30ae78c52097aa85a5f9c24073b17aa4f7e
Q. Zhang <aznyqjai@gmail.com>
2016-10-27 10:29:13 -0400

    uppercaseArray

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-// console.log("playground is up and running");
+// // console.log("playground is up and running");
 
-//day7 morning challenge 
-var num=100;
-while (num>0){
-	if (num%7===0){
-		console.log("Lucky me!");
+// //day7 morning challenge 
+// var num=100;
+// while (num>0){
+// 	if (num%7===0){
+// 		console.log("Lucky me!");
+// 	}
+// 	else {
+// 		console.log(num);
+// 	}
+// 	num--;
+// }
+
+// console.log("*****next problem*****");
+// //day7 shot 2
+
+// for (var i=0; i<=100; i++){
+// 	if (i%9===0){
+// 		console.log("damn Lucky");
+// 	}
+// 	else {
+// 		console.log(i);
+// 	}
+// }
+
+function upArr(arr){
+	var myArr=[];
+
+	for(var i=0; i<arr.length; i++){
+		myArr.push(arr[i].toUpperCase());
 	}
-	else {
-		console.log(num);
-	}
-	num--;
+
+
+	alert(myArr);
+	return myArr;
 }
+upArr(['a','b','c']);
 
-console.log("*****next problem*****");
-//day7 shot 2
 
-for (var i=0; i<=100; i++){
-	if (i%9===0){
-		console.log("damn Lucky");
-	}
-	else {
-		console.log(i);
-	}
-}
+//document.getElementById('output').innerHTML=
